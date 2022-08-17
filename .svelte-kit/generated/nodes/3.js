73406fb5618da9d5bab726516ba39aee65fcaad5
Amelia Wattenberger <wattenberger@gmail.com>
2022-08-17 03:00:38 +0000

    update versions

---
+++ .svelte-kit/generated/nodes/3.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../src/routes/random/+page.svelte";
--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,14 @@
-const sveltePreprocess = require("svelte-preprocess");
+import sveltePreprocess from "svelte-preprocess"
+import adapter from "@sveltejs/adapter-netlify"
 
-module.exports = {
+export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: sveltePreprocess(),
-  adapter: "@sveltejs/adapter-netlify",
   kit: {
-    // By default, `npm run build` will create a standard Node app.
-    // You can create optimized builds for different platforms by
-    // specifying a different adapter
-    adapter: "@sveltejs/adapter-netlify",
-
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
+    adapter: adapter(),
+    alias: {
+      '$components': 'src/components'
+    }
   },
 };
